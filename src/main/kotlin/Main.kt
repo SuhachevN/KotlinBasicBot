e1ca1e1777c9655168e1b9beaf9a0eddb4a2f44b
SuhachevN <tradenoff@gmail.com>
2025-03-29 01:50:06 +0500

    KTB-02-create Dictionary.txt for words

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,22 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, $name!")
+import java.io.File
 
-    for (i in 1..5)
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+
+    val wordsFile: File = File("Dictionary.txt")
+    wordsFile.createNewFile()
+
+    val wordsAndTranslations = listOf(
+        "hello привет",
+        "dog собака",
+        "cat кошка"
+    )
+    wordsFile.writeText(wordsAndTranslations.joinToString("\n"))
+
+    val lines = wordsFile.readLines()
+
+    for (line in lines) {
+        println(line)
+    }
 }
