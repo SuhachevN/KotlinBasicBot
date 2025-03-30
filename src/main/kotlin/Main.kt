62037db26b9710fec8377179b1f21dd72883927f
SuhachevN <tradenoff@gmail.com>
2025-03-31 02:55:16 +0500

    KTB-02 refactor -> add method forEachLine

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,18 +5,8 @@ import java.io.File
 fun main() {
 
     val wordsFile: File = File("Dictionary.txt")
-    wordsFile.createNewFile()
 
-    val wordsAndTranslations = listOf(
-        "hello привет",
-        "dog собака",
-        "cat кошка"
-    )
-    wordsFile.writeText(wordsAndTranslations.joinToString("\n"))
-
-    val lines = wordsFile.readLines()
-
-    for (line in lines) {
+    wordsFile.forEachLine { line ->
         println(line)
     }
 }
