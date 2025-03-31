8e0a4987b7cf0605342f515b4a2cf1e894bafa2c
SuhachevN <tradenoff@gmail.com>
2025-04-01 03:26:47 +0500

    KTB-03 parsed the dictionary file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,31 @@ package org.example
 
 import java.io.File
 
+data class Word(
+    val english: String,
+    val russian: String,
+    var correctAnswersCount: Int = 0,
+)
+
 fun main() {
+    val wordsFile = File("Dictionary.txt")
 
-
-    val wordsFile: File = File("Dictionary.txt")
-
-    val lines = wordsFile.readLines()
+    val dictionary = mutableListOf<Word>()
 
     wordsFile.forEachLine { line ->
-        println(line)
+        val parts = line.split("|")
+
+        if (parts.size >= 3) {
+            val english = parts[0]
+            val russian = parts[1]
+
+            val correctAnswersCount = if (parts.size == 3 && parts[2].isNotEmpty()) parts[2].toInt() else 0
+
+            dictionary.add(Word(english, russian, correctAnswersCount))
+        }
+    }
+
+    for (word in dictionary) {
+        println("${word.english}|${word.russian}|${word.correctAnswersCount}")
     }
 }
