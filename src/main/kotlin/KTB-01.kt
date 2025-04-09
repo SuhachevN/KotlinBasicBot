f76efa6ef5d4fbebdf872c1b697fdd10b89d237a
SuhachevN <tradenoff@gmail.com>
2025-04-10 00:03:43 +0500

    KTB-01 create Dictionary.txt

---
+++ src/main/kotlin/KTB-01.kt
@@ -0,0 +1,34 @@
+package org.example
+
+import java.io.File
+
+data class Word(
+    val english: String,
+    val russian: String,
+    var correctAnswersCount: Int = 0,
+)
+
+fun main() {
+    val wordsFile = File("Dictionary.txt")
+
+    val dictionary = mutableListOf<Word>()
+
+    wordsFile.forEachLine { line ->
+        val parts = line.split("|")
+
+        if (parts.size >= NUMBER_THREE) {
+            val english = parts[0]
+            val russian = parts[1]
+
+            val correctAnswersCount = parts.getOrNull(2)?.toIntOrNull() ?: 0
+
+            dictionary.add(Word(english, russian, correctAnswersCount))
+        }
+    }
+
+    for (word in dictionary) {
+        println("${word.english}|${word.russian}|${word.correctAnswersCount}")
+    }
+}
+
+const val NUMBER_THREE = 3
